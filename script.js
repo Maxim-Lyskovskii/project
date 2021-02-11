3e597ff77a3202b10fe7891582cebb288a24b538
Maxim <maksim_lyskovskii@mail.ru>
2021-02-11 14:08:42 +0300

    second

--- script.js
+++ script.js
@@ -2,6 +2,20 @@
 
 //const answer = prompt("Вам исполнилось 18 лет?" , "");
 //console.log(typeof(answer));
+const NumberofFilms = +prompt('Сколько фильмов вы уже посмотрели?' , '');
 
-const username = "Maxim";
- alert(`Привет,${username} ,как дела?`);
+const personalMovieDB = {
+    count: NumberofFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+ const a = prompt('Один из последних просмотренных фильмов?', ''),
+       b = prompt('На сколько оценте его?',''),
+       c = prompt('Один из последних просмотренных фильмов?', ''),
+       d = prompt('На сколько оценте его?','');
+
+personalMovieDB.movies[a]= b;
+personalMovieDB.movies[c]= d;
+console.log(personalMovieDB);
